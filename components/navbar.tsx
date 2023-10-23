8a5ec0d107570378eeeee76f3aa88d08c6936a09
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2023-10-24 00:15:21 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = async () => {
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
 {/*             <p className="font-bold text-xl ">STORE</p> */}
-                <Image src={Logo} alt="Logo" className="mt-[14px]" width={90} height={60} />
+                <Image src={Logo} alt="Logo" width={90} height={60} />
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
